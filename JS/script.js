47b2d2cb1f961fff0bc0d7e02fc30cdff04e190d
Bassem Y.Helmy <49593830+bassemhlmy@users.noreply.github.com>
2022-03-05 13:14:52 +0200

    Navigation Bar and Landing Pag

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+var lastScrollTop;
+navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", function () {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top = "-100px";
+  } else {
+    navbar.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
+});
