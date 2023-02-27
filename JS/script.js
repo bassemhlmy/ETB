d87b2094cf33a9f34eb7c6fd9bbf247603cc49aa
Bassem Y.Helmy <bassemhlmy@gmail.com>
2023-02-27 20:22:33 +0200

    added pakages,egy_visa info and etb terms pages and tweeking responsive actions and other user interface modifications

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,7 @@ const menuIcon = document.querySelector('.menu-icon');
 const mobileMenu = document.querySelector('.mobile-menu');
 const closeIcone = document.querySelector('.close')
 const backDrop = document.querySelector('.back-drop');
+const navItem = document.querySelector('.nav-item')
 menuIcon.addEventListener('click',function(){
   mobileMenu.classList.toggle('show-mobile-menu');
   backDrop.classList.toggle('hide');
